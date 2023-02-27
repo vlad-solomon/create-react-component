437faa0307903723809de968b3d6cd5925f1ff99
vlad-solomon <vladsolomon720@gmail.com>
2023-02-27 16:09:15 +0200

    Correct export definition

--- crc.sh
+++ crc.sh
@@ -4,5 +4,5 @@ name=$1
 
 mkdir $1
 cd $1 
-echo -e "import \"./$1.scss\";\n\nexport function $1() {\n  return <div>$1</div>;\n}" >> $1.jsx
+echo -e "import \"./$1.scss\";\n\nexport default function $1() {\n  return <div>$1</div>;\n}" >> $1.jsx
 touch $1.scss
